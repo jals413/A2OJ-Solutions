590bafcc97b062959d517b21462863e8dfa7cf96
Jal Thakor <jalsthakor413@gmail.com>
2022-03-30 15:39:06 +0530

    Word Capitalization

---
+++ Word_Capitalization.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    string s; 
+    cin>>s;
+    s[0]=toupper(s[0]);
+    cout<<s;
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+}
